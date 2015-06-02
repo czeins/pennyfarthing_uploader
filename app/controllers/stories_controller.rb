6d90926e3c2fd3c2750f42ad391409812bf4a22c
czeins <claude.zeins@gmail.com>
2015-06-01 18:41:59 -0700

    created story crud

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,10 +1,43 @@
 class StoriesController < ApplicationController
   def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(story_params)
+       if @story.save
+            redirect_to @story
+        else
+            render 'new'
+        end
   end
 
   def show
+    @story = Story.find(params[:id])
   end
 
   def edit
+    @story = Story.find(params[:id])
   end
+
+  def update
+    @story = Story.find(params[:id])
+      if @story.update_attributes(story_params)
+        redirect_to @story
+      else
+        render "edit"
+      end
+  end
+
+  def destroy
+    @story = Story.find(params[:id])
+    @story.destroy
+    redirect_to '/'
+  end
+
+private
+  def story_params
+     params.require(:story).permit(:title, :html)
+  end
+
 end
